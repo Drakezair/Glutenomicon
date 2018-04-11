f7c2bf9466b9c5ad37beb9fe901c4abde17eac50
drakezair <luixdxd@gmail.com>
2018-04-11 06:26:43 +0200

    Angel prefab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,80 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public class GameManager : MonoBehaviour {
 
 	public DataBase DB;
 
-	public bool[] ToFind; //Por encontrar
+	public GameObject[] Angels;
 
-	public bool[] ToCheck; //Lista de confirmacion
+	public Slider ScoreBar;
 
-	public bool[] Finded; //ya encontrados
+	[SerializeField]
+	float MaxScore;
 
-	public GameObject[] Objects; //lista de objetos del nivel
+	public float Score;
 
-	public bool Right;
-
-	public bool Left;
-
-	public float speed;
-
-	public GameObject cameraObject;
 	 
 	// Use this for initialization
 	void Start () {
-
-		Objects = GameObject.FindGameObjectsWithTag ("Object");
-
-		ToCheck = new bool[Objects.Length];
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		moveCamera ();
+
+		ScoreBar.value = Score / MaxScore;
 		
-	}
-
-	public void ReciveList(){
-		for(int i = 0; i < ToCheck.Length; i++){
-
-			ToCheck [i] = Objects[i].GetComponent<Objeto> ().Check;
-
-		}
-	}
-
-	void CheckingList(){
-
-
-	}
-
-	void HandleListChange(){
-
-	
-	}
-
-	public void moveCamera(){
-
-
-		if (cameraObject.gameObject.transform.position.x <= 5.64
-			&& cameraObject.gameObject.transform.position.x <= -5.64) {
-
-			if (Right) {
-				GetComponent<Camera>().gameObject.transform.position = (new Vector2 (1 * speed * Time.deltaTime, 0));
-			}
-		}
-	}
-
-	public void goToRight(){
-		Right = true;
-		Debug.Log ("hola");
-	}
-
-	public void goToLeft(){
-		Left = true;
 	}
 
 
---
+++ Assets/ghosts/angel/angel.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class angel : MonoBehaviour {
+
+	[SerializeField]
+	float speed = 0.2f;
+
+	[SerializeField]
+	float limit;
+
+	[SerializeField]
+	bool Taped = false;
+
+	float X,Y;
+
+	[SerializeField]
+	int Dir;
+
+	public GameManager GM;
+
+	// Use this for initialization
+	void Start () {
+
+		selector (Dir);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (gameObject.transform.localPosition.x <= -28 ||gameObject.transform.localPosition.x >= 1540) {
+			Destroy (gameObject);
+			Debug.Log ("HOLA");
+		}
+
+		gameObject.transform.Translate (X * speed,Y * speed,0);
+		
+	}
+
+
+	public void taped(){
+
+		if (Taped) {
+
+			GM.Score += 10;
+			Destroy (gameObject);
+		} else {
+
+			Taped = true;
+			speed += 0.4f;
+		}
+
+	}
+
+	// MOVIMIENTOS
+
+	public void toLeft(){
+		X = -1;
+		Y = 0;
+	}
+
+
+	public void toRight(){
+		X = 1;
+		Y = 0;
+		gameObject.transform.localScale = new Vector3(-1,1,1);
+	}
+
+	// SELECTOR
+
+	public void selector(int i){
+
+		switch (i) {
+
+		case 0:
+			toLeft ();
+			break;
+		case 1:
+			toRight ();
+			break;
+		}
+	}
+}
+
